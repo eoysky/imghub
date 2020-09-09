e2990a41a5f504bcf7b3e08ef353fb2645dbb5e4
Jonny Chang <58116035+jonny6015@users.noreply.github.com>
2020-09-09 20:57:57 +0800

    优化 pom 删除无用依赖，优化版本引用方式，添加数据库相关资源文件

--- src/main/java/cn/lmsite/imghub/utils/BeanConvertor.java
+++ src/main/java/cn/lmsite/imghub/utils/BeanConvertor.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import cn.hutool.core.util.ArrayUtil;
 import com.alibaba.fastjson.JSON;
-import com.baomidou.mybatisplus.core.toolkit.ArrayUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.util.CollectionUtils;
 
@@ -46,7 +46,7 @@ public class BeanConvertor {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        if (ArrayUtils.isEmpty(ignoreProperties)) {
+        if (ArrayUtil.isEmpty(ignoreProperties)) {
             BeanUtils.copyProperties(source, targetInstance);
         } else {
             BeanUtils.copyProperties(source, targetInstance, ignoreProperties);
